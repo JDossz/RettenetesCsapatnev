ff9641be0d82372b9501a3823d60cf43e6985a7f
Juhasz Dorottya <juhasz.dossz@gmail.com>
2019-06-24 01:20:16 +0200

    újrakezdés - nem fut, pedig csak helyetcserélt 2 function

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function SumErtek()
     }
     document.getElementById("sumErtek").innerHTML = sumErtek;
     console.log(SumErtek());
-}
+};
 
 // A legértékesebb játékos
 function MostValuablePlayer()
@@ -21,19 +21,36 @@ function MostValuablePlayer()
             legertekesebbJatekos = data[i];
         }
     }
-    document.getElementById("legertekesebb").innerHTML = legertekesebbJatekos;
-    console.log(MostValuablePlayer());
-}
+    document.getElementById("legertekesebb").innerHTML = (legertekesebbJatekos.vezeteknev + ' ' + legertekesebbJatekos.utonev);
+    return (legertekesebbJatekos.vezeteknev + ' ' + legertekesebbJatekos.utonev);
+};
+console.log('Legértékesebb játékos: ' + MostValuablePlayer());
+
+// A legkevésbé értékes játékos
+function LeastValuablePlayer()
+{
+    var legkevesbeErtekesJatekos = data[0];
+    for (var i = 0; i < data.length; i++)
+    {
+        if (legkevesbeErtekesJatekos.ertek > data[i].ertek)
+        {
+            legkevesbeErtekesJatekos = data[i];
+        }
+    }
+    document.getElementById("legertektelenebb").innerHTML = (legkevesbeErtekesJatekos.vezeteknev + ' ' +legkevesbeErtekesJatekos.utonev);
+    return (legkevesbeErtekesJatekos.vezeteknev + ' ' + legkevesbeErtekesJatekos.utonev);
+};
+console.log('Legértéktelenebb játékos: ' + LeastValuablePlayer());
 
 /* A játékosok életkorának kiszámítása */
-function CalculateAge(szulido)
+function CalculateAge(pszulido)
 {
-    var age = -1;
+    var age = -1; //hogy tudja, ha valami nem jó
     var dtToday = new Date();
-    var dtBirthdate = new Date(szulido);
+    var dtBirthdate = new Date(pszulido);
     age = dtToday.getFullYear() - dtBirthdate.getFullYear();
     return age; 
-}
+};
 
 /* A user által választott játékos életorának kiválasztása */
 /*
@@ -50,13 +67,49 @@ function CalculateAgeOf(teljesNev)
     {
         result = 
         {
-
-
+            
+            
         }
     }
-}
+};
 */
 
+// A legidősebb játékos:
+function Legidosebb()
+{
+    var legidosebb = '';
+    var legIdosebbJatekosAge = CalculateAge(data[0].szulido);
+    for (var i = 0; i < data.length; i++)
+    {
+        if (CalculateAge(data[i].szulido) > legIdosebbJatekosAge)
+        {
+            legIdosebbJatekosAge = CalculateAge(data[i].szulido);
+        legidosebb = ('A legidősebb játékos ' + data[i].vezeteknev + ' ' + data[i].utonev ', Ő ' + legIdosebbJatekosAge + ' éves, a születési éve pedig ' + data[i].szulido);
+    }
+    }
+    document.getElementById("legidosebb").innerHTML = legidosebb;
+    return legidosebb; //??? hol fut, hol nem, és nem értem, miért
+};
+console.log('Legidősebb játékos: ' + Legidosebb());
+
+// A legfiatalabb játékos:
+function Legfiatalabb()
+{
+    var legfiatalabb = '';
+    var legFiatalabbJatekosAge = CalculateAge(data[0].szulido);
+    for (var i = 0; i < data.length; i++)
+    {
+        if (CalculateAge(data[i].szulido) < legFiatalabbJatekosAge)
+        {
+            legFiatalabbJatekosAge = CalculateAge(data[i].szulido);
+          legfiatalabb = ('A legfiatalabb játékos ' + data[i].vezeteknev + ' ' + data[i].utonev '. Ő ' + legFiatalabbJatekosAge + ' éves, a születési éve pedig ' + data[i].szulido);
+        }
+        document.getElementById("legfiatalabb").innerHTML = legfiatalabb;
+        return legfiatalabb; //??? hol fut, hol nem, és nem értem, miért
+    };
+    console.log('Legidősebb játékos: ' + Legfiatalabb());
+    
+//Magyar játékosok listája:
 function Magyarok()
 {
     var magyarok = [];
@@ -69,7 +122,10 @@ function Magyarok()
     }
     document.getElementById("magyarok").innerHTML = magyarok;
 }
-
+console.log('Magyarok: ' + Magyarok());
+//--> ezt atért kell kivenni, hogy ne jelenjen meg azonnal az érték
+    
+//Külföldi játékosok listája:
 function Kulfoldiek()
 {
     var kulfoldiek = [];
@@ -82,4 +138,5 @@ function Kulfoldiek()
     }
     document.getElementById("kulfoldiek").innerHTML = kulfoldiek;
 }
-
+console.log('Külföldiek: ' + Kulfoldiek());
+//--> ezt atért kell kivenni, hogy ne jelenjen meg azonnal az érték
